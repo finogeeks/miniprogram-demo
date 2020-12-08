7a67079c086880581d08210b32df18cbbce9ddaa
limin <limin@finogeeks.com>
2020-12-08 10:38:16 +0800

    feat: getSystemInfoSyncc

--- pages/API/get-system-info/get-system-info.js
+++ pages/API/get-system-info/get-system-info.js
@@ -12,5 +12,28 @@ Page({
         })
       }
     })
-  }
+  },
+  getSystemInfoSync() {
+    const that = this;
+    try {
+      const res = wx.getSystemInfoSync()
+      console.log(res.model)
+      console.log(res.pixelRatio)
+      console.log(res.windowWidth)
+      console.log(res.windowHeight)
+      console.log(res.language)
+      console.log(res.version)
+      console.log(res.platform)
+      that.setData({
+        systemInfo: res
+      });
+    } catch (e) {
+      // Do something when catch error
+    }
+  },
+  clearInfo() {
+    this.setData({
+      systemInfo: {}
+    })
+  },
 })
