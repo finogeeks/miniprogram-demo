02dd5376f0ffcbcc41b7aed0ab5f77064bd50f33
limin <limin@finogeeks.com>
2020-12-08 11:04:35 +0800

    feat: screen-brightness

---
+++ pages/API/screen-brightness/screen-brightness.js
@@ -0,0 +1,40 @@
+// pages/API/screen-brightness/screen-brightness.js
+Page({
+  data: {
+    screenBrightness: 0
+  },
+
+  onLoad() {
+    this._updateScreenBrightness()
+  },
+
+  changeBrightness(e) {
+    const value = Number.parseFloat(
+      (e.detail.value).toFixed(1)
+    )
+    this.setData({
+      screenBrightness: Number.parseFloat(
+        e.detail.value.toFixed(1)
+      )
+    })
+    wx.setScreenBrightness({
+      value,
+    })
+  },
+
+  _updateScreenBrightness() {
+    wx.getScreenBrightness({
+      success: (res) => {
+        console.log(res);
+        this.setData({
+          screenBrightness: Number.parseFloat(
+            res.value.toFixed(1)
+          )
+        })
+      },
+      fail(err) {
+        console.error(err)
+      }
+    })
+  }
+})
