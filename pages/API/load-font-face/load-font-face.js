3c4303488a2f2270f66908a67f2acf7083a85602
limin <limin@finogeeks.com>
2020-12-08 16:50:29 +0800

    feat: load-font-face

---
+++ pages/API/load-font-face/load-font-face.js
@@ -0,0 +1,35 @@
+// pages/API/load-font-face/load-font-face.js
+Page({
+  data: {
+    fontFamily: 'Bitstream Vera Serif Bold',
+    loaded: false,
+  },
+
+  onLoad() {
+    this.setData({
+      loaded: false
+    })
+  },
+
+  loadFontFace() {
+    const self = this
+    wx.loadFontFace({
+      family: this.data.fontFamily,
+      source: 'url("https://sungd.github.io/Pacifico.ttf")',
+      success(res) {
+        console.log(res.status)
+        self.setData({loaded: true})
+      },
+      fail(res) {
+        console.log(res.status)
+      },
+      complete(res) {
+        console.log(res.status)
+      }
+    })
+  },
+
+  clear() {
+    this.setData({loaded: false})
+  }
+})
