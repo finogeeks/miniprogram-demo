92402d73b27d15002d409c00b82dc63f66e729de
XuPeng <elrichxu@gmail.com>
2020-12-08 13:45:21 +0800

    feat: 添加 radio slider 页面

---
+++ pages/component/slider/slider.js
@@ -0,0 +1,18 @@
+const pageData = {
+  onShareAppMessage() {
+    return {
+      title: 'slider',
+      path: 'pages/component/slider/slider'
+    }
+  },
+}
+
+for (let i = 1; i < 5; ++i) {
+  (function (index) {
+    pageData['slider' + index + 'change'] = function (e) {
+      console.log('slider' + index + '发生change事件，携带值为', e.detail.value)
+    }
+  }(i))
+}
+
+Page(pageData)
