4deb945d05691f7160e6acad8f306c12249fcc0b
limin <limin@finogeeks.com>
2020-12-08 16:50:45 +0800

    feat: request

---
+++ pages/API/request/request.js
@@ -0,0 +1,40 @@
+// pages/API/request/request.js
+const requestUrl = require('../../../config/index').requestUrl
+
+const duration = 2000
+
+Page({
+  makeRequest() {
+    const self = this
+
+    self.setData({
+      loading: true
+    })
+
+    wx.request({
+      url: requestUrl,
+      data: {
+        noncestr: Date.now()
+      },
+      success(result) {
+        wx.showToast({
+          title: '请求成功',
+          icon: 'success',
+          mask: true,
+          duration,
+        })
+        self.setData({
+          loading: false
+        })
+        console.log('request success', result)
+      },
+
+      fail({errMsg}) {
+        console.log('request fail', errMsg)
+        self.setData({
+          loading: false
+        })
+      }
+    })
+  }
+})
