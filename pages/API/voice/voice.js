22d0df1fb3aeece72c75f01e720df5b14ffbb49d
limin <limin@finogeeks.com>
2020-12-09 16:48:34 +0800

    feat: record-voice

---
+++ pages/API/voice/voice.js
@@ -0,0 +1,61 @@
+// pages/API/voice/voice.js
+const util = require('../../../utils/util.js')
+
+let playTimeInterval
+let recordTimeInterval
+Page({
+  data: {
+    recording: false, // 录音中
+    filePath: '',
+  },
+
+  onHide() {
+    if (this.data.recording) {
+      wx.stopRecord() // 结束录音
+    }
+  },
+  startRecord() {
+    this.setData({
+      recording: true,
+    })
+    wx.startRecord({
+      success (res) {
+        const tempFilePath = res.tempFilePath
+        this.setData({
+          filePath: tempFilePath,
+        })
+      }
+    })
+  },
+  stopRecord() {
+    this.setData({
+      recording: false,
+    })
+    wx.stopRecord() // 结束录音
+  },
+  playVoice() {
+    wx.playVoice({
+      filePath: this.data.filePath,
+      success: (res) => {
+        console.log('播放成功', res);
+      },
+      fail: (res) =>{
+        console.log('播放失败', res);
+      }
+    })
+  },
+  stopVoice() {
+    wx.stopVoice({
+      filePath: this.data.filePath,
+      success: (res) => {
+        console.log('播放成功', res);
+      },
+      fail: (res) =>{
+        console.log('播放失败', res);
+      }
+    })
+  },
+  pauseVoice() {
+    wx.pauseVoice();
+  }
+})
